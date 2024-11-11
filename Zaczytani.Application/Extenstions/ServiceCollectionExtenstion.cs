8fdb13c15f95be2ddc67118e62af721594e3a3ae
Mateusz Lengiewicz <mateusz.lengiewicz111@gmail.com>
2024-11-11 13:58:26 +0100

    [PT2024ZACZ-91] - Add automapper package in Application module

--- Zaczytani.Application/Extenstions/ServiceCollectionExtenstion.cs
+++ Zaczytani.Application/Extenstions/ServiceCollectionExtenstion.cs
@@ -9,6 +9,7 @@ public static class ServiceCollectionExtension
     public static void AddApplication(this IServiceCollection services)
     {
         var applicationAssembly = typeof(ServiceCollectionExtension).Assembly;
+        services.AddAutoMapper(applicationAssembly);
         services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(applicationAssembly));
 
         services.AddValidatorsFromAssembly(applicationAssembly)
