1fb6b9c8e032903ef3e2845a2d619a58293352e7
Divel5577 <gitpdulkowski@gmail.com>
2024-11-18 16:41:11 +0100

    [PT2024ZACZ-92] - changed GetBookDetailsQuery to a record

--- Zaczytani.Application/Shared/Queries/GetBookDetailsQuery.cs
+++ Zaczytani.Application/Shared/Queries/GetBookDetailsQuery.cs
@@ -1,29 +1,25 @@
 ﻿using AutoMapper;
 using MediatR;
 using Zaczytani.Application.Dtos;
-using Zaczytani.Domain.Repositories;
 using Zaczytani.Application.Exceptions;
+using Zaczytani.Domain.Repositories;
 
 namespace Zaczytani.Application.Shared.Queries;
 
-public class GetBookDetailsQuery(Guid BookId) : IRequest<BookDto>
+public record GetBookDetailsQuery(Guid BookId) : IRequest<BookDto>
 {
-    public Guid BookId { get; }
-
     private class GetBookDetailsQueryHandler(IBookRepository BookRepository, IMapper Mapper)
+        : IRequestHandler<GetBookDetailsQuery, BookDto>
     {
-        private readonly IBookRepository _bookRepository;
-        private readonly IMapper _mapper;
-
         public async Task<BookDto> Handle(GetBookDetailsQuery request, CancellationToken cancellationToken)
         {
-            var book = await _bookRepository.GetByIdAsync(request.BookId);
+            var book = await BookRepository.GetByIdAsync(request.BookId);
             if (book == null)
             {
                 throw new NotFoundException($"Book with ID {request.BookId} was not found.");
             }
 
-            return _mapper.Map<BookDto>(book);
+            return Mapper.Map<BookDto>(book);
         }
     }
 }
