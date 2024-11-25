d39583118e667548eb86e1e680f4952e7c211efe
Mateusz Lengiewicz <mateusz.lengiewicz111@gmail.com>
2024-11-25 23:06:13 +0100

    [PT2024ZACZ-21] - add enum translations

---
+++ Zaczytani.Domain/DescriptionEnumConver/DescriptionEnumConverter.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel;
+using System.Reflection;
+using System.Text.Json.Serialization;
+using System.Text.Json;
+
+namespace Zaczytani.Domain.DescriptionEnumConver;
+
+public class DescriptionEnumConverter<T> : JsonConverter<T> where T : struct, Enum
+{
+    public override T Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        var value = reader.GetString();
+        foreach (var field in typeof(T).GetFields(BindingFlags.Public | BindingFlags.Static))
+        {
+            var description = field.GetCustomAttribute<DescriptionAttribute>()?.Description;
+            if (description != null && description.Equals(value, StringComparison.OrdinalIgnoreCase))
+            {
+                return (T)field.GetValue(null);
+            }
+        }
+
+        return Enum.Parse<T>(value, true);
+    }
+
+    public override void Write(Utf8JsonWriter writer, T value, JsonSerializerOptions options)
+    {
+        var field = typeof(T).GetField(value.ToString());
+        var description = field?.GetCustomAttribute<DescriptionAttribute>()?.Description ?? value.ToString();
+        writer.WriteStringValue(description);
+    }
+}
---
+++ Zaczytani.Domain/DescriptionEnumConver/EnumHelper.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+using System.Reflection;
+
+namespace Zaczytani.Domain.DescriptionEnumConver;
+
+public static class EnumHelper
+{
+    public static List<string> GetEnumDescriptions<T>() where T : Enum
+    {
+        return typeof(T).GetFields(BindingFlags.Public | BindingFlags.Static)
+                        .Select(field => field.GetCustomAttribute<DescriptionAttribute>()?.Description ?? field.Name)
+                        .ToList();
+    }
+}
