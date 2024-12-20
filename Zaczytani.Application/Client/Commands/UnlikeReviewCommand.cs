0a712ac9ab47b5322f60035007b224fc8bc84608
Divel5577 <gitpdulkowski@gmail.com>
2024-12-20 14:43:24 +0100

    [PT2024ZACZ-131] - adjusted exceptions

--- Zaczytani.Application/Client/Commands/UnlikeReviewCommand.cs
+++ Zaczytani.Application/Client/Commands/UnlikeReviewCommand.cs
@@ -1,5 +1,6 @@
 ﻿using MediatR;
 using Zaczytani.Application.Filters;
+using Zaczytani.Domain.Exceptions;
 using Zaczytani.Domain.Repositories;
 
 public record UnlikeReviewCommand(Guid ReviewId) : IRequest, IUserIdAssignable
@@ -15,7 +16,7 @@ public record UnlikeReviewCommand(Guid ReviewId) : IRequest, IUserIdAssignable
         public async Task Handle(UnlikeReviewCommand request, CancellationToken cancellationToken)
         {
             var review = await _reviewRepository.GetReviewByIdAsync(request.ReviewId, cancellationToken)
-                ?? throw new KeyNotFoundException("Review not found");
+                ?? throw new NotFoundException("Review not found");
 
             if (!review.Likes.Contains(request.UserId))
                 throw new InvalidOperationException("User has not liked this review.");
