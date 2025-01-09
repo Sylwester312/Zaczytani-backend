535b008c0f8c1ecd0b1b6de9d2f8ab704cee2533
Mateusz Lengiewicz <mateusz.lengiewicz111@gmail.com>
2025-01-09 17:50:02 +0100

    [PT2024ZACZ-159] - Add EP for changing user's password

---
+++ Zaczytani.Application/Shared/Validators/ChangePasswordCommandValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using Zaczytani.Application.Shared.Commands;
+using Zaczytani.Domain.Constants;
+
+namespace Zaczytani.Application.Shared.Validators;
+
+public class ChangePasswordCommandValidator : AbstractValidator<ChangePasswordCommand>
+{
+    public ChangePasswordCommandValidator()
+    {
+        RuleFor(dto => dto.CurrentPassword)
+           .NotEmpty().WithMessage("Current password cannot be empty.");
+
+        RuleFor(dto => dto.NewPassword)
+            .NotEmpty().WithMessage("New password cannot be empty.")
+            .MinimumLength(8).WithMessage("New password must be at least 8 characters long.")
+            .Matches(Resources.PasswordRegex).WithMessage("New password must include special characters and numbers");
+    }
+}
